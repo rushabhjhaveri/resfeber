c337d1890fa743d38dbfb9207b36de439b7e0590
Rushabh Jhaveri <rjhaveri98@gmail.com>
2018-08-16 21:24:44 -0400

    Flask web server up and running, homepage template rendered and works on web server!

---
+++ app.py
@@ -0,0 +1,20 @@
+# Import flask module. 
+from flask import Flask, render_template
+
+# Create app using flask. 
+app = Flask(__name__)
+
+# Define basic route `/`. 
+@app.route("/")
+
+# Define basic route request handler. 
+def main():
+	return render_template('homepage.html')
+
+'''
+Check if executed file is the main program. 
+If it is, run the app. 
+'''
+if __name__ == "__main__": 
+	app.run()
+
